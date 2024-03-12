3eb7891c24783cb51da4a8ec5f5c76bfdee8e5e1
steven <ssteven075@gmail.com>
2024-03-12 12:56:44 +0700

    update git ignore

--- invoke.sh
+++ invoke.sh
@@ -16,4 +16,4 @@ export CORE_PEER_ADDRESS=localhost:7051
 
 peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C ${CHANNEL_NAME} -n basic --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"InitLedger","Args":[]}'
 
-peer chaincode query -C ${CHANNEL_NAME} -n basic -c '{"Args":["GetAllAssets"]}'
+peer chaincode query -C ${CHANNEL_NAME} -n basic -c '{"Args":["GetAllAssets"]}' | jq '.'
